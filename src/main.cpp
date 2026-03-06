db92f6a8b04d1780b70ce2f4f3a2ec2557bd1cae
Adriano Patane <adriano.patane@outlook.com>
2026-03-06 10:41:21 +0100

    Servo test Workshop 2

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 
 // drivers
 #include "DebounceIn.h"
+#include "Servo.h"
 
 bool do_execute_main_task = false; // this variable will be toggled via the user button (blue button) and
                                    // decides whether to execute the main task or not
@@ -44,6 +45,31 @@ int main()
     AnalogIn ir_analog_in(PC_2);    // create AnalogIn object to read in the infrared distance sensor
                                     // 0...3.3V are mapped to 0...1
 
+    // servo
+    Servo servo_D0(PB_D0);
+    Servo servo_D1(PB_D1);
+
+    // minimal pulse width and maximal pulse width obtained from the servo calibration process
+    // futuba S3003
+    float servo_D0_ang_min = 0.035000f; // careful, these values might differ from servo to servo
+    float servo_D0_ang_max = 0.125000f;
+    // reely S0090
+    float servo_D1_ang_min = 0.0325f;
+    float servo_D1_ang_max = 0.1175f;
+
+    // servo.setPulseWidth: before calibration (0,1) -> (min pwm, max pwm)
+    // servo.setPulseWidth: after calibration (0,1) -> (servo_D0_ang_min, servo_D0_ang_max)
+    servo_D0.calibratePulseMinMax(servo_D0_ang_min, servo_D0_ang_max);
+    servo_D1.calibratePulseMinMax(servo_D1_ang_min, servo_D1_ang_max);
+
+    // default acceleration of the servo motion profile is 1.0e6f
+    servo_D0.setMaxAcceleration(0.5f);
+    
+    float servo_input = 0.0f;
+    int servo_counter = 0; // define servo counter, this is an additional variable
+                       // used to command the servo
+    const int loops_per_seconds = static_cast<int>(ceilf(1.0f / (0.001f * static_cast<float>(main_task_period_ms))));
+
     // start timer
     main_task_timer.start();
 
@@ -54,7 +80,7 @@ int main()
         // --- code that runs every cycle at the start goes here ---
 
         // print to the serial terminal
-        printf("IR distance mV: %f \n", ir_distance_mV);
+        //printf("IR distance mV: %f \n", ir_distance_mV);
 
         if (do_execute_main_task) {
 
@@ -65,6 +91,24 @@ int main()
 
             // visual feedback that the main task is executed, setting this once would actually be enough
             led1 = 1;
+
+            // enable the servos
+            if (!servo_D0.isEnabled())
+                servo_D0.enable();
+            if (!servo_D1.isEnabled())
+                servo_D1.enable();
+
+            // command the servos
+            servo_D0.setPulseWidth(servo_input);
+            servo_D1.setPulseWidth(servo_input);
+
+            // calculate inputs for the servos for the next cycle
+            if ((servo_input < 1.0f) &&                     // constrain servo_input to be < 1.0f
+                (servo_counter % loops_per_seconds == 0) && // true if servo_counter is a multiple of loops_per_second
+                (servo_counter != 0))                       // avoid servo_counter = 0
+                servo_input += 0.1f;
+            servo_counter++;
+
         } else {
             // the following code block gets executed only once
             if (do_reset_all_once) {
@@ -75,6 +119,11 @@ int main()
                 // reset variables and objects
                 led1 = 0;
                 ir_distance_mV = 0.0f;
+                // reset variables and objects
+                led1 = 0;
+                servo_D0.disable();
+                servo_D1.disable();
+                servo_input = 0.0f;
             }
         }
 
@@ -82,6 +131,9 @@ int main()
         user_led = !user_led;
 
         // --- code that runs every cycle at the end goes here ---
+
+        // print to the serial terminal
+        printf("Pulse width: %f \n", servo_input);
 
         // read timer and make the main thread sleep for the remaining time span (non blocking)
         int main_task_elapsed_time_ms = duration_cast<milliseconds>(main_task_timer.elapsed_time()).count();
